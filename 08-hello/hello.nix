bfaadbd62d044510dd94ffe5192c4b4ffb590551
ivanbrennan <ivan.brennan@gmail.com>
2018-01-28 16:23:42 -0500

    08-hello (autotools, mkDerivation)

--- 08-hello/hello.nix
+++ 08-hello/hello.nix
@@ -1,9 +1,8 @@
-with (import <nixpkgs> {});
-derivation {
+let
+  pkgs = import <nixpkgs> {};
+  mkDerivation = import ./autotools.nix pkgs;
+in
+  mkDerivation {
   name = "hello";
-  builder = "${bash}/bin/bash";
-  args = [ ./hello_builder.sh ];
-  inherit gnutar gzip gnumake gcc binutils coreutils gawk gnused gnugrep;
   src = ./hello-2.10.tar.gz;
-  system = builtins.currentSystem;
 }
